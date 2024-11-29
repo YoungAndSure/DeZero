b15a8bb4eba93258a6a666a99caeaeb7e1697eb0
YoungAndSure <yangshuorex@foxmail.com>
2024-11-29 11:13:27 +0800

    slice utest

--- v3/utest/function_test.py
+++ v3/utest/function_test.py
@@ -14,36 +14,6 @@ from dezero.config import *
 from dezero.user_defined_func import *
 from dezero.util import _dot_var, _dot_func, get_dot_graph, plot_dot_graph
 
-class UtilTest(unittest.TestCase) :
-  def test_dot_var(self) :
-    x0 = Variable(np.array(1.0))
-    x1 = Variable(np.array(2.0))
-    y = x0 + x1
-    print(_dot_var(y))
-    print(_dot_func(y.creator))
-  
-  def test_get_dot_graph(self) :
-    x0 = Variable(np.array(1.0), "x0")
-    x1 = Variable(np.array(2.0), "x1")
-    y = x0 + x1
-    y.name = "y"
-    plot_dot_graph(y, verbose=True, to_file="add.png")
-  
-  def test_get_dot_graph2(self) :
-    x = Variable(np.array(1.0))
-    y = Variable(np.array(1.0))
-    z = goldstein(x, y)
-    plot_dot_graph(z, verbose=True, to_file="goldstein.png")
-  
-  def test_graph_taylor_sin(self) :
-    x0 = Variable(np.array(np.pi / 4))
-    y0 = taylor_sin(x0)
-    x1 = Variable(np.array(np.pi / 4))
-    y1 = sin(x1)
-    self.assertTrue(np.allclose(y0.data, y1.data))
-    plot_dot_graph(y0, verbose=True, to_file="taylor_sin.png")
-    plot_dot_graph(y1, verbose=True, to_file="sin.png")
-
 class AddTest(unittest.TestCase) :
   def test_forward(self) :
     x0 = Variable(np.array(1.0))
@@ -257,7 +227,6 @@ class AddTest(unittest.TestCase) :
     y = rosenbrock(x0, x1)
     self.assertEqual(y.data, 401.0)
     y.backward(retain_grad=True)
-    plot_dot_graph(y, verbose=True, to_file="rosenbrock.png")
     self.assertEqual(x0.grad, -2.0)
     self.assertEqual(x1.grad, 400.0)
 
