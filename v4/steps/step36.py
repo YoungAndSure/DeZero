cc4f7db62a7777bcce7a8c796bbd1de738897720
YoungAndSure <yangshuorex@foxmail.com>
2024-12-04 11:54:13 +0800

    step36

---
+++ v4/steps/step36.py
@@ -0,0 +1,19 @@
+#! python3
+
+if '__file__' in globals() :
+  import os, sys
+  sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from dezero import *
+
+x = Variable(np.array(2.0))
+y = x ** 2
+
+y.backward(create_graph=True)
+gx = x.grad
+x.cleargrad()
+
+z = gx ** 3 + y
+z.backward()
+
+print(x.grad.data)
