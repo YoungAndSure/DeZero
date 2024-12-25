7f13af3fdb5df751bb6554a929b2b7094a299510
YoungAndSure <yangshuorex@foxmail.com>
2024-12-25 10:46:50 +0800

    step49

---
+++ v5/step/step49.py
@@ -0,0 +1,47 @@
+#! python3
+
+if '__file__' in globals() :
+  import os, sys
+  sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import dezero as C
+import dezero.layer as L
+import dezero.model as M
+import dezero.optimizer as O
+import dezero.datasets as D
+
+import numpy as np
+import math
+
+dataset = D.Spiral(train=True)
+
+max_epoch = 300
+batch_size = 30
+lr = 1.0
+hidden_size = 10
+
+model = M.MLP(full_connect_layer_size=(hidden_size, 3))
+optimizer = O.SDG(lr).setup(model)
+data_size = len(dataset)
+iter_size = math.ceil(data_size / batch_size)
+
+for epoch in range(max_epoch) :
+  index = np.random.permutation(data_size)
+  loss_sum = 0
+
+  for j in range(iter_size) :
+    batch_start = j * batch_size
+    batch_end = (j + 1) * batch_size
+    batch_index = index[batch_start:batch_end]
+    batch = [dataset[index] for index in batch_index]
+    batch_x = np.array([x[0] for x in batch])
+    batch_t = np.array([t[1] for t in batch])
+    
+    predict_y = model(batch_x)
+    loss = C.softmax_cross_entropy_simple(predict_y, batch_t)
+    model.cleargrad()
+    loss.backward(retain_grad=True)
+    optimizer.update()
+
+    loss_sum += float(loss.data) * len(batch_t)
+  print('epoch %d, loss %.2f' % (epoch + 1, loss_sum / data_size))
