ff68f3717c335cb96c1371bfeb6a4870e9c791a8
YoungAndSure <yangshuorex@foxmail.com>
2025-01-07 08:56:32 +0800

    update utils from external

--- v5/external/utils.py
+++ v5/external/utils.py
@@ -47,3 +47,14 @@ def pair(x):
         return x
     else:
         raise ValueError
+
+def show_progress(block_num, block_size, total_size):
+    bar_template = "\r[{}] {:.2f}%"
+
+    downloaded = block_num * block_size
+    p = downloaded / total_size * 100
+    i = int(downloaded / total_size * 30)
+    if p >= 100.0: p = 100.0
+    if i >= 30: i = 30
+    bar = "#" * i + "." * (30 - i)
+    print(bar_template.format(bar, p), end='')
